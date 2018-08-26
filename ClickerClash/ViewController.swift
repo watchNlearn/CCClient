033af03673a77269aed17ba0d157e0db7a576b35
Caleb Lee <caleblee@dhcp-39-1-133.dhcpint.csulb.edu>
2018-08-26 00:03:15 -0700

    Initial Commit

---
+++ ClickerClash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickerClash
+//
+//  Created by Caleb Lee on 8/26/18.
+//  Copyright © 2018 WithoutAnyLimits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
