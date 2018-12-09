e3d96d2dcb1287f91d171e79e7ea98e05c1daf6d
Caleb Lee <28caleblee@gmail.com>
2018-12-09 12:24:38 -0800

    Added Terms of Use

---
+++ ClickerClash/TermsOfUseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TermsOfUseViewController.swift
+//  ClickerClash
+//
+//  Created by Caleb Lee on 12/8/18.
+//  Copyright © 2018 WithoutAnyLimits. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfUseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
